0a5768b0055a97ed68b47412787470c6aef744b8
Chong Xuan Liang <xxl-0011@hotmail.com>
2019-01-04 21:08:11 +0800

    completed lab1

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -29,5 +29,15 @@ public class LeapYear {
             }
         }
     }
+
+    /** Checks boolean if year is leap year */
+    public static boolean isLeapYear(int year) {
+        if (year % 400 == 0) {
+            return true;
+        } else if (year % 4 == 0 && year % 100 != 0) {
+            return true;
+        }
+        return false;
+    }
 }
 
